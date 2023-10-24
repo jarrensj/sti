76039a77cd3bbc71477e267a1dc0c76346393c8d
Jarren San Jose <sanjose.jarren@gmail.com>
2023-10-23 20:23:53 -0700

    get the notes for the user

--- components/User.tsx
+++ components/User.tsx
@@ -1,27 +1,47 @@
 'use client'
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "" 
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || ""
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-async function record(name:string, phoneNumber:string, note:string) {
-    const { data, error } = await supabase
-      .from('notes')
-      .insert([
-        { name, phone_number: phoneNumber, note },
-      ])
-      .select()
+async function getNotes(user) {
+  try {
+    let { data: notes, error } = await supabase.from('notes').select('*').eq('phone_number', user);
+    console.log(notes);
+    if (error) {
+      throw error;
+    }
+    return notes;
+  } catch (error) {
+    console.error('Error fetching notes:', error.message);
+    return [];
+  }
 }
 
 const User = ({ user }) => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    async function fetchNotes(user) {
+      const notesData = await getNotes(user);
+      setNotes(notesData);
+    }
+    fetchNotes(user);
+  }, [user]);
 
   return (
     <div>
-      <h1>hi {user}</h1>
-    </div>    
+      <h1>Hi {user}</h1>
+      <h2>Notes:</h2>
+      <ul>
+        {notes.map((note, index) => (
+          <li key={index}>{note.note}</li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
