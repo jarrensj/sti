398a1db5450a33b6e8b4eabffd2a18d62b67f6ec
Jarren San Jose <sanjose.jarren@gmail.com>
2023-10-23 19:06:57 -0700

    add link to submit page and check page on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
      hi
+     <ul>
+      <li>
+        <Link href="/submit">go to submit page </Link>
+      </li>
+      <li>
+        <Link href="/check">go to check page</Link>
+      </li>
+    </ul>
     </main>
   )
 }
