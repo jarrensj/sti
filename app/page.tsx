cabd82eddb9baabebee275e5c79d0e449dc6447d
Jason Takahashi <jmtakahashi@gmail.com>
2023-10-25 15:03:32 -0700

    add tailwind classes and text to app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,35 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-     hi
-     <ul>
-      <li>
-        <Link href="/submit">go to submit page </Link>
-      </li>
-      <li>
-        <Link href="/check">go to check page</Link>
-      </li>
-    </ul>
+    <main className="flex min-h-screen flex-col items-center p-24 ">
+      <div className="container bg-blue-300">
+        <h1>Tracker</h1>
+        <p className="mb-6">
+          Welcome! We&apos;re helping you reduce the risk of sti transmission,
+          in turn helping you stay healthy and, last but not least, MAKE THE
+          WORLD A BETTER PLACE.
+        </p>
+        <ul className="flex flex-col gap-4">
+          <li>
+            <Link
+              className="block bg-blue-100 text-center max-w-sm rounded-lg shadow-lg p-12  border border-gray-200 dark:border-gray-700"
+              href="/submit"
+            >
+              Submit An Exposure
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="block  bg-blue-100 text-center max-w-sm rounded-lg shadow-lg p-12  border border-gray-200 dark:border-gray-700"
+              href="/check"
+            >
+              Check Your Exposure
+            </Link>
+          </li>
+        </ul>
+      </div>
     </main>
-  )
+  );
 }
