bbb03194206ac0baaa7ba16aea89399e3a2a98c0
Jason Takahashi <jmtakahashi@gmail.com>
2023-10-25 15:09:37 -0700

    add text and tailwind classes to pages/submit.tsx

--- pages/submit.tsx
+++ pages/submit.tsx
@@ -1,12 +1,28 @@
-import Form from '../components/Form'
-import { Roboto } from 'next/font/google';
-const roboto = Roboto({ weight: "400", subsets: ['latin'] })
+import Form from "../components/Form";
+import { Roboto } from "next/font/google";
+const roboto = Roboto({ weight: "400", subsets: ["latin"] });
+
+import Link from "next/link";
 
 export default function Submit() {
   return (
-    <main className={`${roboto.className} flex min-h-screen flex-col items-center p-24`}>
-     submit
-     <Form />
+    <main
+      className={`${roboto.className} flex min-h-screen flex-col items-center p-24`}
+    >
+      <div className="container bg-blue-300">
+        <div id="header">
+          <Link href="/">← Back</Link>
+        </div>
+
+        <h1>Submit Potential Exposure</h1>
+        <p className="mb-6">
+          Make the world a better place by submitting a potential exposure you
+          had! Whether your the giver, or the receiver, letting your partner
+          know they could be at risk without letting them know who you are is a
+          great way to keep that pristine image, but also be responsible!
+        </p>
+        <Form />
+      </div>
     </main>
-  )
+  );
 }
