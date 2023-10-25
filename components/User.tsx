703b966fc65f4541c9d7e2162b2bc426ce32c5cb
Jason Takahashi <jmtakahashi@gmail.com>
2023-10-25 15:51:02 -0700

    add tailwind classes to components/User.tsx

--- components/User.tsx
+++ components/User.tsx
@@ -1,33 +1,36 @@
-'use client'
+"use client";
 
-import React, { useState, useEffect } from 'react';
-import { createClient } from '@supabase/supabase-js'
-import { Roboto } from 'next/font/google';
-const roboto = Roboto({ weight: "400", subsets: ['latin'] })
+import React, { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { Roboto } from "next/font/google";
+const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "" 
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || ""
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || "";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-async function getNotes(user:string) {
+async function getNotes(user: string) {
   try {
-    let { data: notes, error } = await supabase.from('notes').select('*').eq('phone_number', user);
+    let { data: notes, error } = await supabase
+      .from("notes")
+      .select("*")
+      .eq("phone_number", user);
     console.log(notes);
     if (error) {
       throw error;
     }
     return notes;
   } catch (error) {
-    console.error('Error fetching notes:', error);
+    console.error("Error fetching notes:", error);
     return [];
   }
 }
 
-const User = ({ user }: UserProps ) => {
+const User = ({ user }: UserProps) => {
   const [notes, setNotes] = useState<any[] | null>([]);
 
   useEffect(() => {
-    async function fetchNotes(user:string) {
+    async function fetchNotes(user: string) {
       const notesData = await getNotes(user);
       setNotes(notesData);
     }
@@ -35,12 +38,14 @@ const User = ({ user }: UserProps ) => {
   }, [user]);
 
   return (
-    <div className={roboto.className}>
-      <h1>Hi {user}</h1>
+    <div className={`${roboto.className} bg-blue-100 rounded-md p-6`}>
+      <h1 className="mt-0">Hi {user}</h1>
       <h2>Notes:</h2>
       <ul>
         {notes?.map((note, index) => (
-          <li key={index}>{note.note}</li>
+          <li className="my-3" key={index}>
+            {note.note}
+          </li>
         ))}
       </ul>
     </div>
@@ -52,4 +57,3 @@ type UserProps = {
 };
 
 export default User;
-
