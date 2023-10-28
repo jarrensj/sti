d757504109c2f8988af6dfac96e8b90409bf0c24
Jarren San Jose <sanjose.jarren@gmail.com>
2023-10-27 20:31:40 -0700

    fix indentation

--- components/STI.tsx
+++ components/STI.tsx
@@ -1,11 +1,10 @@
 const STI = () => {
   return (
     <div>
-        <h3>Someone left you a message mentioning STIs. We are not doctors and this is not medical advice but if you would like to check out some resources about STIs, here are some: </h3>
-        <p>Disclaimer: We are not doctors or medical professionals. This is not health advice. This is not medical advice. </p>
+      <h3>Someone left you a message mentioning STIs. We are not doctors and this is not medical advice but if you would like to check out some resources about STIs, here are some: </h3>
+      <p>Disclaimer: We are not doctors or medical professionals. This is not health advice. This is not medical advice. </p>
     </div>        
   );
 };
 
 export default STI;
-
