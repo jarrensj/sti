7617402855f86855beb7148df87c66db506a0a8d
Jarren San Jose <sanjose.jarren@gmail.com>
2023-10-30 14:41:29 -0700

    edit sti resources section

--- components/STI.tsx
+++ components/STI.tsx
@@ -2,6 +2,14 @@ const STI = () => {
   return (
     <div>
         <h3>Someone left you a message mentioning STIs. We are not doctors and this is not medical advice but if you would like to check out some resources about STIs, here are some: </h3>
+        <ul>
+          <li>
+            STI Resource 1
+          </li>
+          <li>
+            STI Resource 2
+          </li>
+        </ul>
         <p>Disclaimer: We are not doctors or medical professionals. This is not health advice. This is not medical advice. </p>
     </div>        
   );
