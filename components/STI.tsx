0025be0e5d0ffbb2d988d2deab8e9c4f61380389
Chloe Ng <chloegng@gmail.com>
2023-10-30 14:48:29 -0700

    sti resources

--- components/STI.tsx
+++ components/STI.tsx
@@ -4,10 +4,24 @@ const STI = () => {
         <h3>Someone left you a message mentioning STIs. We are not doctors and this is not medical advice but if you would like to check out some resources about STIs, here are some: </h3>
         <ul>
           <li>
-            STI Resource 1
+            <a href="https://www.who.int/news-room/fact-sheets/detail/sexually-transmitted-infections-(stis)?gclid=EAIaIQobChMI4tGMm96eggMVAiytBh0wCg2aEAAYAyAAEgI-1_D_BwE" target="_blank">
+              World Health Organization: Sexually transmitted infections (STIs)
+            </a>
           </li>
           <li>
-            STI Resource 2
+            <a href="https://www.cdc.gov/std/default.htm" target="_blank">
+              CDC
+            </a>
+          </li>
+          <li>
+            <a href="https://www.cdc.gov/std/treatment/default.htm" target="_blank">
+              CDC STD Treatment and Screening
+            </a>
+          </li>
+          <li>
+            <a href="https://www.thewellproject.org/hiv-information/sexually-transmitted-infections-or-diseases-stis-or-stds?gclid=EAIaIQobChMI4tGMm96eggMVAiytBh0wCg2aEAAYASAAEgI71vD_BwE" target="_blank">
+              thewellproject
+            </a>
           </li>
         </ul>
         <p>Disclaimer: We are not doctors or medical professionals. This is not health advice. This is not medical advice. </p>
